7e06f22504a7e2e5c2dcb2bbcc3d8804f658c245
Roman <romanmaslov200@gmail.com>
2024-09-08 12:26:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReadLineType
+{
+    internal class Program
+    {
+        static ProverkaType proverkaType = new ProverkaType();
+
+        static decimal dm; // 128b
+        static double dobl; // 64b
+        static long l; // 64b
+        static ulong ul; //64b
+        static int i; // 32b
+        static uint ui; // 32b
+        static float f; // 32b
+        static short sh; // 16b
+        static ushort ush; // 16b
+        static byte b; // 8b
+        static sbyte sb; //8b
+
+        static void Main(string[] args)
+        {
+            dm = proverkaType.DecimalType(dm, "Введите decimal => ");
+            dobl = proverkaType.DoubleType(dobl, "Введите double => ");
+            l = proverkaType.LongType(l, "Введите long => ");
+            ul = proverkaType.UlongType(ul, "Введите ulong => ");
+            i = proverkaType.IntType(i, "Введите int => ");
+            ui = proverkaType.UintType(ui, "Введите uint => ");
+            f = proverkaType.FloatType(f, "Введите float => ");
+            sh = proverkaType.ShortType(sh, "Введите short => ");
+            ush = proverkaType.UshortType(ush, "Введите ushort => ");
+            b = proverkaType.ByteType(b, "Введите byte => ");
+
+            Console.WriteLine($"\ndecimal = {dm}\ndouble = {dobl}\nlong = {l}\nulong = {ul}\nint = {i}\nuint = {ui}\nfloat = {f}\nshort = {sh}\nushort = {ush}\nbyte = {b}");
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProverkaType.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReadLineType
+{
+    internal class ProverkaType
+    {
+        public decimal DecimalType(decimal i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (decimal.TryParse(Console.ReadLine().Replace(".", ","), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public double DoubleType(double i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (double.TryParse(Console.ReadLine().Replace(".", ","), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public long LongType(long i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (long.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public ulong UlongType(ulong i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (ulong.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public int IntType(int i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (int.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public uint UintType(uint i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (uint.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public float FloatType(float i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (float.TryParse(Console.ReadLine().Replace(".", ","), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public short ShortType(short i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (short.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public ushort UshortType(ushort i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (ushort.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public byte ByteType(byte i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (byte.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+
+        public sbyte SbyteType(sbyte i, string s)
+        {
+        start:
+
+            Console.Write(s);
+            if (sbyte.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else
+            {
+                goto start;
+            }
+        }
+    }
+}
